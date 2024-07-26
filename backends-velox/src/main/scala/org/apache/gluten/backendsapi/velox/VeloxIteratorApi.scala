80bb95164d8aec0cc118589bbaffb17b1b922968
j7nhai <j7nhai.chen@gmail.com>
2024-07-26 15:25:12 +0800

    show local file node pb.

--- backends-velox/src/main/scala/org/apache/gluten/backendsapi/velox/VeloxIteratorApi.scala
+++ backends-velox/src/main/scala/org/apache/gluten/backendsapi/velox/VeloxIteratorApi.scala
@@ -94,6 +94,12 @@ class VeloxIteratorApi extends IteratorApi with Logging {
 
     splitInfos.zipWithIndex.map {
       case (splitInfos, index) =>
+        splitInfos.map(_.asInstanceOf[LocalFilesNode].toProtobuf).foreach { pb =>
+          // scalastyle:off
+          println(s"local file pb: ${pb.toString}")
+          // scalastyle:on
+        }
+
         GlutenPartition(
           index,
           planByteArray,
